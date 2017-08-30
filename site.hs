2d65964e355d4165e7ee18d5c3730da80396b9d2
spencerpeters <spencer@goebelpeters.com>
2017-08-29 21:54:09 -0700

    Revert "Merge branch 'updateCSS'"
    This reverts commit 64ba2ca2d01c1119a30c1c1eddee18360b4110e8, reversing
    changes made to bfdcaa5845ee8ab9830fc4a87445527186f47a95.

--- site.hs
+++ site.hs
@@ -1,14 +1,11 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend, (<>))
+import           Data.Monoid (mappend)
 import           Hakyll
 import           Data.List
 import           System.FilePath
 import           Hakyll.Core.Configuration
 import           System.Process
-import           Text.Pandoc.Options
-import qualified Data.Map as M
-
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -27,10 +24,9 @@ main = hakyllWith config $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompilerWith myReaderOptions myWriterOptions
+        compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
@@ -96,22 +92,3 @@ config = Configuration
         | otherwise                    = False
       where
         fileName = takeFileName path
-
-myReaderOptions :: ReaderOptions
-myReaderOptions = defaultHakyllReaderOptions
-
-myWriterOptions :: WriterOptions
-myWriterOptions = defaultHakyllWriterOptions {
-      writerReferenceLinks = True
-    , writerHtml5 = True
-    , writerHighlight = True
-    , writerHTMLMathMethod = MathJax "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js"
-    }
-
--- mathCtx :: Context String
--- mathCtx = field "mathjax" $ \item -> do
---     metadata <- getMetadata $ itemIdentifier item
---     return lookupString "mathjax"
--- --     return $ if "mathjax" `M.member` metadata
--- --              then "<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script>"
--- --              else ""
